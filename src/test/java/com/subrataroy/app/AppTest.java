9c85bb67fd06f56bbb682dcea4c93663342cba40
Subrata Roy <subrata.roy2194@gmail.com>
2025-06-24 14:29:39 +0000

    Add unit tests, update pom.xml and buildspec.xml

---
+++ src/test/java/com/subrataroy/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.subrataroy.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true, "This is a sample test");
+    }
+}
--- target/nextwork-7d-devops-project/index.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Hello Subrata!</h2>
-<p>This is my Maven web application working!</p>
-<p>This is Exciting!</p>
-</body>
-</html>
